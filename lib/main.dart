16c82dae6be1f2f740524d49ee7146df5e211001
Dr Jessica Muirhead <github@jessica.digital>
2024-05-24 09:50:14 +0100

    Flutter Bootcamp Lesson 41

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+   MyApp()
   );
 }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            height: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            width: 100.0,
+            child: Text('Hello'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
